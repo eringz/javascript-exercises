1a404899c8ac1abdd854e240015eb0d67369e1be
roNGarciaSantos <68483083+Eringz@users.noreply.github.com>
2023-03-07 11:40:20 +0800

    update

--- 28-Cryptocurrency/server.js
+++ 28-Cryptocurrency/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
-const axios = require('axios');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+const axios = require('axios');
+
+const app = express();
+// more new code:
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
 
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,22 +19,61 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/',function(req, res){
+    req.session.page = 1;
     res.render('index');
 });
 
-app.post('/page',function(req, res){
-    console.log(req.body);
-    res.render('index', {})
-});
-
-app.post('/page', function(req, res){
-    let url = "https://api.coingecko.com/api/v3/exchanges?page=2&per_page=10";
+app.get('/next', function(req, res){
+    console.log(req.session.page);
+    req.session.page += 1;
+    let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
     // console.log(req.url);
     // use the axios .get() method - provide a url and chain the .then() and .catch() methods
     axios.get(url)
     .then(data => {
         // log the data before moving on! 
-        console.log(data.data);
+        // console.log(data);
+        // rather than rendering, just send back the json data!
+        res.json(data.data);
+    })
+    .catch(error => {
+        // log the error before moving on!
+        // console.log(error);
+        res.json('error ko '+error);
+    })
+});
+
+app.get('/previous', function(req, res){
+    console.log(req.session.page);
+    req.session.page -= 1;
+    let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
+    // console.log(req.url);
+    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+    axios.get(url)
+    .then(data => {
+        // log the data before moving on! 
+        // console.log(data);
+        // rather than rendering, just send back the json data!
+        res.json(data.data);
+    })
+    .catch(error => {
+        // log the error before moving on!
+        // console.log(error);
+        res.json('error ko '+error);
+    })
+});
+
+app.get('/top', function(req, res){
+    // console.log(req.session.page);
+    // req.session.page -= 1;
+    // let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=100";
+    let url = "https://api.coingecko.com/api/v3/exchanges?page=1&per_page=100";
+    // console.log(req.url);
+    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+    axios.get(url)
+    .then(data => {
+        // log the data before moving on! 
+        // console.log(data);
         // rather than rendering, just send back the json data!
         res.json(data.data);
     })
