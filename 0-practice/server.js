f577bde97296be9d3ca254a9e00fdfae3af834ef
roNGarciaSantos <68483083+Eringz@users.noreply.github.com>
2023-03-09 18:18:25 +0800

    update

--- 0-practice/server.js
+++ 0-practice/server.js
@@ -1,52 +1,27 @@
 const express = require('express');
 const app = express();
-let path = require('path');
+app.use(express.static(__dirname + "/public"));
+const server = app.listen(1337);
+const io = require('socket.io')(server);
+var counter = 0;
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('index');
+
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
+    
+io.on('connection', function (socket) { //2
+  
+  socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
+  socket.on('thankyou', function (data) { //7
+    console.log(data.msg); //8 (note: this log will be on your server's terminal)
+  });
+    
 });
 
 // app.listen(port = 8000, function(){
-//     console.log('Running in localhost at port', port);
-// });
-// es5 style
-var anonES5 = function(parameter){                      
-    return parameter + 5;                                               
-}
-// arrow functions
-const anonES6 = parameter => parameter + 5;  
-// curly brackets are not required if there is only one expression
-// parentheses are not required if there is only one parameter
-// the return is implicit with just one line
-const twoParams = (parameter1, parameter2) => {   
-    parameter1 += 5;                  
-    return parameter1 + parameter2;               
-} 
-// with more parameters, parentheses are required  
-// with more lines of code, curly brackets are required    
-
-const axios = require('axios');
-app.get('/people', function(req, res){
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    axios.get(url)
-    .then(data => {
-        // log the data before moving on! 
-        console.log(data);
-        // rather than rendering, just send back the json data!
-        res.json(data.data);
-    })
-    .catch(error => {
-        // log the error before moving on!
-        console.log(error);
-        res.json(error);
-    })
-});
-
-app.listen(port = 8000, function(){
-    console.log('Running in localhost at port', port);
-});
-
-
+//   console.log('Running in localhost at port '+ port);
+// })
--- 28-Cryptocurrency/server.js
+++ 28-Cryptocurrency/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 const axios = require('axios');
-
 const app = express();
-// more new code:
+var session = require('express-session');
+
 app.use(session({
   secret: 'keyboardkitteh',
   resave: false,
@@ -23,68 +22,58 @@ app.get('/',function(req, res){
     res.render('index');
 });
 
-app.get('/next', function(req, res){
-    console.log(req.session.page);
-    req.session.page += 1;
-    let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
-    // console.log(req.url);
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    axios.get(url)
-    .then(data => {
-        // log the data before moving on! 
-        // console.log(data);
-        // rather than rendering, just send back the json data!
-        res.json(data.data);
-    })
-    .catch(error => {
-        // log the error before moving on!
-        // console.log(error);
-        res.json('error ko '+error);
-    })
+app.get('/platform',function(req, res){
+    req.session.page = 1;
+    res.render('platform');
 });
 
+app.get('/exchange',function(req, res){
+    req.session.page = 1;
+    res.render('exchange');
+});
+
+
 app.get('/previous', function(req, res){
-    console.log(req.session.page);
-    req.session.page -= 1;
-    let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
-    // console.log(req.url);
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
-    axios.get(url)
-    .then(data => {
-        // log the data before moving on! 
-        // console.log(data);
-        // rather than rendering, just send back the json data!
-        res.json(data.data);
-    })
-    .catch(error => {
-        // log the error before moving on!
-        // console.log(error);
-        res.json('error ko '+error);
-    })
+    if(req.session.page > 1){
+        console.log(req.session.page);
+        req.session.page -= 1;
+        let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
+        axios.get(url)
+        .then(data => {
+            res.json(data.data);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+    }
 });
 
 app.get('/top', function(req, res){
-    // console.log(req.session.page);
-    // req.session.page -= 1;
-    // let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=100";
+    req.session.page = 1;
     let url = "https://api.coingecko.com/api/v3/exchanges?page=1&per_page=100";
-    // console.log(req.url);
-    // use the axios .get() method - provide a url and chain the .then() and .catch() methods
     axios.get(url)
     .then(data => {
-        // log the data before moving on! 
-        // console.log(data);
-        // rather than rendering, just send back the json data!
         res.json(data.data);
     })
     .catch(error => {
-        // log the error before moving on!
-        // console.log(error);
-        res.json('error ko '+error);
+        res.json(error);
+    })
+});
+
+app.get('/next', function(req, res){
+    req.session.page += 1;
+    console.log(req.session.page);
+    let url = "https://api.coingecko.com/api/v3/exchanges?page="+req.session.page+"&per_page=10";
+    axios.get(url)
+    .then(data => {
+        res.json(data.data);
+    })
+    .catch(error => {
+        res.json(error);
     })
 });
 
 app.listen(port = 8000, function(){
-    console.log('Running in localhost at port', port);
+    console.log('Running in localhost at port'+port);
 });
 
