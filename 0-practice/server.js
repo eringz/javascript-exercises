8d82ad944c24ab585ee1dbe9a92683c15e51dafb
roNGarciaSantos <68483083+Eringz@users.noreply.github.com>
2023-03-17 05:09:20 +0800

    update

--- 0-practice/server.js
+++ 0-practice/server.js
@@ -1,48 +1,63 @@
 /*
     1. Have the NodeJS server render views/index.ejs that has the html content for the client whenever the client requests "/" 
+        routings/index.js
+        app.get("/", function(req, res){
+            res.render("index", {});
+        });
+
     2. In the client codes, have a javascript code that asks the user for their name. Store the user input in a variable called name
+        <script>
+            var name = prompt("What is your name");
+        </script>
+
     3. Have the client EMIT to the server an event called "got_a_new_user" and pass 'name' to the server.
-    4. Have the server LISTEN for an event called "got_a_new_user". When this event gets triggered, have the server BROADCAST an event called 'new_user' to the client with the name of the new user attached to the event.
+        <script>
+            var name = prompt("What is your name");
+            io = io.connect();
+            io.emit("got_a_new_user", {name:name});
+        </script>
+
+    4. Have the server LISTEN for an event called "got_a_new_user". When this event gets triggered, 
+    4.1have the server BROADCAST an event called 'new_user' to the client with the name of the new user attached to the event.
+        app.io.route('got_a_new_user', function(req){
+            app.io.broadcast('new_user', {new_user_name: req.data.name});
+        });
+    4.2 we store the name/session_id of the new user in a variable/array/hash called users;
+        
+        var users = {};
+    
+    4.3 to the person who sent this request, we EMIT an event called "existing_users" with all the users data.
+
+        app.io.route("got_a_new_user", function(req){
+            app.io.broadcast("new_user", {new_user_name: req.data.name});
+            req.io.emit();
+        });
+
     5. Have the client LISTEN for an event called 'new_user' and when this event gets triggered, have jQuery render a new box with the new user's name.
+        <script>
+            var name = prompt("What is your name");
+            io = io.connect();
+            io.emit("got_a_new_user", {name:name});
+            
+            io.on("new_user", function(data){
+                //render this information into HTML
+            });
+        </script>
     6. Wait... But this does not detect when a user disconnects from the socketIO connection. To do this, we need to have the server LISTEN for an event called 'disconnect' and when this event gets triggered, broadcast an event called 'disconnect_user' to all clients.
     7. We need to have the client LISTEN for an event called 'disconnect_user' and remove any html box associated with this user.
 
 */
 const express = require('express');
 const app = express();
-var session = require('express-session');   
 
 app.use(express.static(__dirname + '/static'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(session({
-  secret: 'keyboardkitteh',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}));
 
+const server = app.listen(8000);
 
-app.get('/',function(req, res){
-    res.render('index');
+app.get('/', function(req, res){
+    res.render('index', {});
 });
 
-const server = app.listen(port = 8000, function(){
-    console.log('Running in localhost at port '+port);
-});
 
-const io = require('socket.io')(server);
-
-
-
-io.on('connection', function(socket){
-    
-    var users = [];
-    socket.on('got_a_new_user', function(req){
-        console.log('server listen to: ', req.name);
-        console.log('session id: '+ req.session.id);
-        socket.emit('new_user', {new_user_name: req.name});
-        users.push({name: req.name, id: 1}) ;
-    });
-    io.emit('existing_user', {users: users});
-});
